1716ca1d178e04db8044439c2f0a572a089149d4
Samuel <titiloye1@gmail.com>
2026-06-03 20:35:43 +0100

    database error fixed

--- backend/database/db.js
+++ backend/database/db.js
@@ -8,7 +8,7 @@ types.setTypeParser(1114, function(val) {
 });
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'uiges_db',
   user: process.env.DB_USER || 'postgres',
